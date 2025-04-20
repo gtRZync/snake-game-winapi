fee538471f85354926cc39a9f16abc4230f0b381
Myson Dio <sendorfbg@gmail.com>
2025-04-20 04:06:58 -0300

    changed colors to define and also added some extern variables

--- header/config.h
+++ header/config.h
@@ -25,34 +25,46 @@ typedef enum tagDirections
     DOWN,
 } DIRECTIONS;
 
+typedef enum tagGameStates
+{
+    MENU,
+    PLAYING,
+    PAUSED,
+    GAMEOVER
+}GAMESTATE;
+
 extern int32_t timer_intervalUID; 
 extern int32_t screen_height, screen_width;
 extern int32_t score;
+extern int clickedX, clickedY;
+extern boolean has_clicked;
 
 //----------------------------Color section -------------------------------------
-extern const COLORREF red;
-extern const COLORREF green;
-extern const COLORREF blue;
-extern const COLORREF white;
-extern const COLORREF black;
-extern const COLORREF gray;
-extern const COLORREF yellow;
-extern const COLORREF cyan;
-extern const COLORREF magenta;
-extern const COLORREF orange;
-extern const COLORREF purple;
-extern const COLORREF teal;
-extern const COLORREF navy;
-extern const COLORREF pink;
-extern const COLORREF gold;
-extern const COLORREF darkOrange;
-extern const COLORREF lightOrange;
-extern const COLORREF copperRed;
-extern const COLORREF SNAKE_HEAD_COLOR;
-extern const COLORREF SNAKE_BODY_COLOR;
+#define red              RGB(255, 0, 0)
+#define green            RGB(0, 255, 0)
+#define blue             RGB(0, 0, 255)
+#define white            RGB(255, 255, 255)
+#define black            RGB(0, 0, 0)
+#define gray             RGB(128, 128, 128)
+#define yellow           RGB(255, 255, 0)
+#define cyan             RGB(0, 255, 255)
+#define magenta          RGB(255, 0, 255)
+#define orange           RGB(255, 165, 0)
+#define darkOrange       RGB(252, 178, 79)
+#define lightOrange      RGB(251, 193, 78)
+#define purple           RGB(128, 0, 128)
+#define teal             RGB(0, 128, 128)
+#define navy             RGB(0, 0, 128)
+#define pink             RGB(255, 192, 203)
+#define gold             RGB(255, 215, 0)
+#define copperRed        RGB(183, 80, 48)
+#define SNAKE_HEAD_COLOR RGB(85, 107, 47)    // Olive
+#define SNAKE_BODY_COLOR RGB(139, 69, 19)    // SaddleBrown
+#define champagneSilver  RGB(230, 232, 234)
+#define MENU_BG          RGB(36,94,99)
 
 //------------------- Defining scales ---------------------------------
-//! maybe in tha future
+//! maybe in da future
 #define DPI_SCALE_100 96
 #define DPI_SCALE_125 120
 #define DPI_SCALE_150 144
